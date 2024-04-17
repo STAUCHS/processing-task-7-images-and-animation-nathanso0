ceac7bbf42986cf0a1bfd1d88b3d04fad9b5b309
nathanso0 <nathan.so1165@gmail.com>
2024-04-16 20:16:56 -0400

    ask teacher

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,9 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-
+  float intx = 200;
+  float inty = 0;
+  float fltspeed = 0;
   public void settings() {
     size(400, 400);
   }
@@ -11,7 +13,18 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
-    
+    fltspeed = 1;
+    intx += 0;
+    inty += fltspeed;
+    background(210,255,173);
+    fill(0,0,0);
+    ellipse(200, 200, 40, 40);
+    fill(255,255,255);
+    ellipse(intx, inty, 40, 40);
+    if (inty == 200){
+    fltspeed = 0;
+    background(210,255,173);
+    }
   }
 
 }
